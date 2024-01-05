b791e4fe58282bf188948eb619b228a85858aeac
maredin <mringer@bk.ru>
2024-01-06 02:40:37 +0500

    burger

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
+import burger from "./modules/burger.js";
 import popup from "./modules/popup.js";
 
 window.addEventListener('DOMContentLoaded', () => {
-    popup('.poppup', '.btn', false);
-    
+    burger();
+
 });
